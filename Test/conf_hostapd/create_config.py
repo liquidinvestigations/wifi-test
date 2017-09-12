4267b9b9828640702cfebd394535012c9a60cf18
AlSegno <nathan@alsegno.com>
2017-09-13 00:58:58 +0300

    initial commit

---
+++ Test/conf_hostapd/create_config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from hostapdconf.parser import HostapdConf
+from hostapdconf import helpers as ha
+import subprocess
+
+def create_hostapd_conf(ssid, password, interface):
+    """
+
+    Create a new hostapd.conf with the given ssid, password, interface.
+
+    Overwrites the current config file.
+
+    """
+    subprocess.call(['touch', './test_conf_supplicant.hostapd.conf'])
+    conf = HostapdConf('./test_conf_supplicant.hostapd.conf')
+
+    # set some common options
+    ha.set_ssid(conf, ssid)
+    ha.reveal_ssid(conf)
+    ha.set_iface(conf, interface)
+    ha.set_driver(conf, ha.STANDARD)
+    ha.set_channel(conf, 2)
+    ha.enable_wpa(conf, passphrase=password, wpa_mode=ha.WPA2_ONLY)
+    ha.set_country(conf, 'ro')
+
+    # my hostapd doesn't like the default values of -1 here, so we set some
+    # dummy values
+    conf.update({'rts_threshold': 0, 'fragm_threshold': 256})
+
+    print("writing configuration")
+    conf.write()
+
+if __name__ == '__main__':
+    print("Creating conf file...")
+    create_hostapd_conf('test_conf_supplicant', 'password', 'wlan0')
---
+++ Test/test_conf_supplicant/test_conf_supplicant.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import pytest
+from xprocess import ProcessStarter
+
+
+@pytest.fixture
+def modprobe_hwsim():
+    """
+    pytest fixture to install/uninstall the mac80211_hwsim module before/after
+    test runs.
+    """
+
+    print("Installing mac80211_hwsim module...")
+    subprocess.call(['sudo', 'modprobe', 'mac80211_hwsim'])
+
+    yield
+
+    print("Uninstalling mac80211_hwsim module...")
+    subprocess.call(['sudo', 'modprobe', '-r', 'mac80211_hwsim'])
+
+@pytest.fixture
+def start_AP(xprocess):
+    """
+    pytest-xprocess fixture that runs hostapd and waits for the AP to be enabled.
+    """
+
+    class Starter(ProcessStarter):
+        """
+        TODO: we need to bring this config file with us. Currently this will only be
+              successful if run from /Test
+        """
+        args = ['sudo', 'hostapd', os.getcwd() + '/conf_hostapd/test_conf_supplicant.hostapd.conf']
+        pattern = "wlan0: AP-ENABLED"
+
+    logfile = xprocess.ensure("AP", Starter)
+    print("Started AP...")
+    return True
+
+@pytest.fixture
+def supplicant_interface():
+    pass
+
+def test_conf_supplicant(modprobe_hwsim, start_AP):
+    """
+    With the mac80211_hwsim module installed and the AP running, tests that the
+    client can find and connect to the AP.
+
+    TODO: (Pending API implementation) this should implemented via:
+           1 - An API call to set the SSID/password.
+           2 - An API call to check that it connected.
+    """
+    pass
